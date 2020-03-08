8fb62c3465b0148e35ed8ce89fa24077373bd984
Eric Ambiel <eric.ambiel@gmail.com>
2020-03-08 11:55:42 -0300

    Implementando: Download Contratos

--- src/app/dialogs/file/file.dialog.component.ts
+++ src/app/dialogs/file/file.dialog.component.ts
@@ -8,11 +8,18 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./file.dialog.component.scss']
 })
 export class FileDialogComponent {
+  displayedColumns = ['numAditivo',
+                      'dataInsert'];
 
   constructor(public dialogRef: MatDialogRef<FileDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public dataContrato: any, public contratoDataService: DataService) { }
+              @Inject(MAT_DIALOG_DATA) public dataContrato: any,
+              public contratoDataService: DataService) { }
 
-  onNoClick(): void {
+  onSairClick(): void {
     this.dialogRef.close();
+  }
+
+  downloadFile(): void{
+    // TODO metodo que baixa o arquivo na maquina do usuário.
   }
 }
