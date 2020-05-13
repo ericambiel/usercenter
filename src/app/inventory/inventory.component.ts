e0d23c22dff77ea1bc656123a0245ad8ba2d4d89
ericambiel <eric.ambiel@gmail.com>
2020-05-13 11:53:03 -0300

    Implementando: View Patrimônio.

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-inventory',
-  templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  templateUrl: 'inventory.component.html',
+  styleUrls: ['inventory.component.scss']
 })
-export class InventoryComponent implements OnInit {
 
-  constructor() { }
+export class InventoryComponent implements OnInit {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.contactForm = fb.group({
+      designation: '11',
+      patrimonyNumberFrom: '1',
+      patrimonyNumberTo: '1',
+      subNumberFrom: '1',
+      subNumberTo: '1',
+      inventoryNumberFrom: '1',
+      inventoryNumberTo: '1',
+      costCenterFrom: '1',
+      costCenterTo: '1',
+      incorporationDateFrom: new Date('01-25-1990'),
+      incorporationDateTo: new Date('02-15-2000')
+    });
+  }
 
   ngOnInit(): void {
   }
--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,12 +1,56 @@
 import { NgModule } from '@angular/core';
+import { InventoryComponent } from './inventory.component';
+import { InventoryRoutingModule } from './inventory-routing.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
 import { CommonModule } from '@angular/common';
 
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+    InventoryComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    CommonModule,
+
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    // MatMomentDateModule,
+    MatTooltipModule,
+
+    InventoryRoutingModule
+  ],
+  exports: [ InventoryComponent ]
 })
 export class InventoryModule { }
